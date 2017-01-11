656d9379d708d8793d637adf53c19974e8da7f32
alatella <alessandro.latella@gmail.com>
2017-01-11 20:23:31 +0100

    Aggiunto routes

---
+++ js/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+angular.module('cv', ['ui.router'])
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+
+        // route for the home page
+            .state('app', {
+            url: '/',
+            views: {
+
+                'content': {
+                    templateUrl: 'views/tbd.html',
+                    controller: 'tbd'
+                },
+
+            }
+
+        })
+
+        // route for the aboutus page
+        .state('app.aboutme', {
+            url: 'aboutme',
+            views: {
+                'content@': {
+                    templateUrl: 'views/tbd.html',
+                    controller: 'tbd'
+                }
+            }
+        })
+
+        $urlRouterProvider.otherwise('/');
+    });
