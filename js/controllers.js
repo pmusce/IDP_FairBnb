82edb1cfa88a792b2ad4b817f2cd898fac1899cb
Pasquale Muscettola <pasquale.muscettola@gmail.com>
2017-01-14 15:53:40 +0100

    Broken news list

--- js/controllers.js
+++ js/controllers.js
@@ -1,1 +1,13 @@
+angular.
+  module('fairBnB').
+  component('newsList', {
+    templateUrl: '../views/news-list.template.html',
+    controller: function NewsListController($http) {
+      var self = this;
 
+      $http.get('../data/news.json').then(function(response) {
+        self.news = response.data;
+      });
+      
+    }
+  });
