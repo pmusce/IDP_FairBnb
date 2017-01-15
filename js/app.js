0a428635138f5bddf15becce98c7160d942a8cc0
alatella <alessandro.latella@gmail.com>
2017-01-15 16:47:57 +0100

    Commit

--- js/app.js
+++ js/app.js
@@ -4,25 +4,25 @@ var fairBnB = angular.module('fairBnB', ['ui.bootstrap', 'newsList', 'discussion
 
 // Define the `PhoneListController` controller on the `fairBnB` module
 fairBnB.controller('PageController', function PageController($scope) {
-	$scope.user = {
-		name: 'Pasquale Muscettola',
-		username: 'polenta',
-		role: 'admin',
-		isAdmin: function() {
-			return this.role == 'admin';
-		},
-		isGuest: function() {
-			return this.role == 'guest';
-		},
-		switchRole: function () {
-			this.role = this.oppositeRole().toLowerCase();
-		},
-		oppositeRole: function () {
-			if (this.role == 'host')
-				return 'Admin';
-			if (this.role == 'admin')
-				return 'Host';
-			console.error("Invalid user role");
-		}
-	};
+    $scope.user = {
+        name: 'Pasquale Muscettola',
+        username: 'polenta',
+        role: 'admin',
+        isAdmin: function() {
+            return this.role == 'admin';
+        },
+        isGuest: function() {
+            return this.role == 'guest';
+        },
+        switchRole: function() {
+            this.role = this.oppositeRole().toLowerCase();
+        },
+        oppositeRole: function() {
+            if (this.role == 'host')
+                return 'Admin';
+            if (this.role == 'admin')
+                return 'Host';
+            console.error("Invalid user role");
+        }
+    };
 });
