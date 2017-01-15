f9a7764a06394ef07723e9b1b29b1728a4a0e7f3
Pasquale Muscettola <pasquale.muscettola@gmail.com>
2017-01-15 16:17:14 +0100

    Switch user roles

--- js/app.js
+++ js/app.js
@@ -7,12 +7,22 @@ fairBnB.controller('PageController', function PageController($scope) {
 	$scope.user = {
 		name: 'Pasquale Muscettola',
 		username: 'polenta',
-		role: 'guest',
+		role: 'admin',
 		isAdmin: function() {
 			return this.role == 'admin';
 		},
 		isGuest: function() {
 			return this.role == 'guest';
+		},
+		switchRole: function () {
+			this.role = this.oppositeRole().toLowerCase();
+		},
+		oppositeRole: function () {
+			if (this.role == 'host')
+				return 'Admin';
+			if (this.role == 'admin')
+				return 'Host';
+			console.error("Invalid user role");
 		}
 	};
 });
