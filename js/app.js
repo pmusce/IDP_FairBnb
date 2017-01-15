4bcaecc4b77260d4039ac52c09b07aadeff3f0b7
Pasquale Muscettola <pasquale.muscettola@gmail.com>
2017-01-15 16:00:24 +0100

    Dynamic user roles

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-angular.module('fairBnB', ['ui.bootstrap', 'newsList', 'discussionList', 'projectsList', 'tabsCtrle']);
+// Define the `fairBnB` module
+var fairBnB = angular.module('fairBnB', ['ui.bootstrap', 'newsList', 'discussionList', 'projectsList', 'tabsCtrle']);
+
+
+// Define the `PhoneListController` controller on the `fairBnB` module
+fairBnB.controller('PageController', function PageController($scope) {
+	$scope.user = {
+		name: 'Pasquale Muscettola',
+		username: 'polenta',
+		role: 'guest',
+		isAdmin: function() {
+			return this.role == 'admin';
+		},
+		isGuest: function() {
+			return this.role == 'guest';
+		}
+	};
+});
