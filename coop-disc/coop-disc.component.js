f1efd9f9daaaaee9ed64b3dc0d33f62ea859e9a7
Pasquale Muscettola <pasquale.muscettola@gmail.com>
2017-01-22 16:52:35 +0100

    Landing fix

--- coop-disc/coop-disc.component.js
+++ coop-disc/coop-disc.component.js
@@ -2,12 +2,25 @@ angular.
 	module('discussionList').
   component('discussionList', {
     templateUrl: 'coop-disc/coop-disc.template.html',
-    controller: function discussionListController($http) {
+    controller: function discussionListController($http, $scope, $uibModal) {
       var self = this;
 
       $http.get('../data/coop-disc.json').then(function(response) {
         self.discussions = response.data;
       });
 
+      $scope.createDiscussion = function() {
+          $uibModal.open({
+            animation: true,
+            templateUrl: 'coop-disc/new-discuss.template.html',
+            controller: 'NewDiscussionModalInstanceCtrl',
+            controllerAs: '$ctrl'
+          });
+      };
     }
-  });
+  })
+  .controller('NewDiscussionModalInstanceCtrl', function ($scope, $uibModalInstance, $state) {
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+});
