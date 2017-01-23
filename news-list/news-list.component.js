3556b9baf6116133d5539d76b396cc94a2425f13
Pasquale Muscettola <pasquale.muscettola@gmail.com>
2017-01-23 12:55:58 +0100

    Fix modals

--- news-list/news-list.component.js
+++ news-list/news-list.component.js
@@ -2,7 +2,7 @@ angular.
 module('newsList').
 component('newsList', {
   templateUrl: 'news-list/news-list.template.html',
-  controller: function NewsListController($scope, $http, user) {
+  controller: function NewsListController($scope, $uibModal, $http, user) {
     var self = this;
     this.user = user;
     this.news = [];
@@ -18,6 +18,21 @@ component('newsList', {
       }, 0);
       console.log(self.user.unread);
     };
+
+    $scope.createNews = function() {
+      $uibModal.open({
+          animation: true,
+          templateUrl: 'create-news-modal.html',
+          controller: 'CreateNewsModalInstanceCtrl',
+          controllerAs: '$ctrl',
+          size: 'lg',
+          resolve: {
+            project: function() {
+              return $scope.project;
+            }
+          }
+      });
+    }
 
     $http.get('../data/news.json').then(function(response) {
       self.news = response.data;
@@ -42,4 +57,10 @@ component('newsList', {
       $scope.newsOpened = false;
     }
   }
+})
+.controller('CreateNewsModalInstanceCtrl', function ($scope, project, $uibModalInstance, $state) {
+  $scope.project = project;
+  $scope.cancel = function () {
+      $uibModalInstance.dismiss('cancel');
+  };
 });
