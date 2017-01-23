bf528a18aff76d38bc93ea34ae58d372bded1b85
Pasquale Muscettola <pasquale.muscettola@gmail.com>
2017-01-23 18:11:34 +0100

    Discussion page

--- coop-disc/coop-disc.component.js
+++ coop-disc/coop-disc.component.js
@@ -2,25 +2,29 @@ angular.
 module('discussionList').
 component('discussionList', {
   templateUrl: 'coop-disc/coop-disc.template.html',
-  controller: function discussionListController($http, $scope, $uibModal) {
+  controller: function discussionListController($http, $scope, discussionsService, discussionDetailService, $uibModal) {
     var self = this;
-    $scope.detailOpened = 'false';
-
-    $http.get('../data/coop-disc.json').then(function(response) {
-      self.discussions = response.data;
-    });
+    $scope.service = discussionDetailService;
+    $scope.discussions = discussionsService;
 
     $scope.createDiscussion = function() {
-      $uibModal.open({
+      console.log('YEAG');
+      console.log($scope.discussions.list);
+      var modalInstance = $uibModal.open({
         animation: true,
         templateUrl: 'coop-disc/new-discuss.template.html',
         controller: 'NewDiscussionModalInstanceCtrl',
         controllerAs: '$ctrl'
       });
+
+      modalInstance.result.then(function (new_discussion) {
+        discussionsService.add(new_discussion);
+      });
     };
 
     $scope.openDiscussion = function(discussion) {
-      $scope.detailOpened = 'true';
+      $scope.service.discussion = discussion;
+      $scope.service.detailOpened = 'true';
     };
   }
 })
@@ -29,11 +33,49 @@ component('discussionList', {
   $scope.cancel = function () {
     $uibModalInstance.dismiss('cancel');
   };
+
+  $scope.submit = function() {
+    new_discussion = {
+      author: user.username,
+      usr_img: user.image,
+      type: 'open',
+      title: $scope.title,
+      body: $scope.body
+    }
+
+    $uibModalInstance.close(new_discussion);
+
+
+  };
 })
 .component('discussionDetail', {
   templateUrl: 'coop-disc/discussion-detail.template.html',
-  controller: function discussionDetailController($http, $scope) {
-    var self = this;
-    $scope.detailOpened = 'false';
+  controller: function discussionDetailController($http, discussionDetailService, $scope) {
+    var self = this; 
+    $scope.discussion = discussionDetailService.discussion;
+    $scope.back = function() {
+      discussionDetailService.detailOpened = 'false';
+    }
   }
+})
+.factory('discussionsService', function($http) {
+  self = this;
+  self.discussions = {
+    list: [],
+    add: function(new_discussion) {
+      this.list.push(new_discussion);
+    }
+  };
+
+  $http.get('../data/coop-disc.json').then(function(response) {
+      self.discussions.list = response.data;
+  });
+  
+  return self.discussions;
+})
+.factory('discussionDetailService', function() {
+  return {
+    discussion: undefined,
+    detailOpened: 'false'
+  };
 });
